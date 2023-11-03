0290017b19d2d30ec2eec166a004d767a7b824a6
wuxinkui <wuxinkui@git>
2023-11-03 14:22:53 +0800

    init wxk

---
+++ packages/wxk-cli/bin/copy.js
@@ -0,0 +1,56 @@
+const copydir = require('copy-dir')
+const fs = require('fs')
+const path = require('path')
+const Mustache = require('mustache');
+
+function mkdirGuard(target) {
+  try{
+    fs.mkdirSync(target)
+  } catch (e) {
+    mkdirp(target)
+    function mkdirp(dir) {
+      if(fs.existsSync(dir)) return true
+      const dirname = path.dirname(dir)
+      mkdirp(dirname)
+      fs.mkdirSync(dir)
+    }
+  }
+}
+
+function copyDir(from, to, options) {
+  mkdirGuard(to)
+  copydir.sync(from, to, options)
+}
+function checkMkdirExists(path) {
+  return fs.existsSync(path)
+}
+
+function copyFile(from, to) {
+  const buffer = fs.readFileSync(from)
+  const parentPath = path.dirname(to)
+
+  mkdirGuard(parentPath)
+
+  fs.writeFileSync(to, buffer)
+}
+function readTemplate(path, data = {}) {
+  const str = fs.readFileSync(path, { encoding: 'utf8' })
+  return Mustache.render(str, data)
+}
+function copyTemplate(from, to, data = {}) {
+  if (path.extname(from) !== '.tpl') {
+    return copyFile(from, to);
+  }
+  const parentToPath = path.dirname(to);
+  mkdirGuard(parentToPath);
+  fs.writeFileSync(to, readTemplate(from, data));
+}
+
+exports.copyTemplate = copyTemplate
+
+exports.readTemplate = readTemplate
+
+exports.mkdirGuard = mkdirGuard
+exports.copyDir = copyDir
+exports.checkMkdirExists = checkMkdirExists
+exports.copyFile = copyFile
